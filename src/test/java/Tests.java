ce585a635cb0026ee5eea162aef5bde94b1c1a7d
Alexander Demin <quilberion@gmail.com>
2024-04-08 15:48:50 +0300

    add comment

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -9,7 +9,7 @@ public class Tests {
         assertEquals("Standard constructor returns wrong denominator", 1, standard.getDenominator());
     }
     @Test
-    public void testCreatRational() {
+    public void testCreateRational() {
         Rational s = new Rational(-1, 2);
         assertEquals(-1, s.getNumerator());
         assertEquals(2, s.getDenominator());
@@ -19,5 +19,34 @@ public class Tests {
         Rational s = new Rational(1,2).multiply(new Rational(1, 3));
         assertEquals(1, s.getNumerator());
         assertEquals(6, s.getDenominator());
+    }
+    @Test
+    public void testEquals() {
+        boolean equals = new Rational(1, 2).equals(new Rational(1,2));
+        assertTrue("Not equal", equals);
+    }
+    @Test
+    public void testLess() {
+        boolean less = new Rational(1,3).less(new Rational(1,2));
+        assertTrue(less);
+    }
+    @Test
+    public void testLessOrEqual() {
+        boolean less = new Rational(1,3).lessOrEqual(new Rational(1,2));
+        assertTrue(less);
+        boolean equal = new Rational(1,2).lessOrEqual(new Rational(1,2));
+        assertTrue(equal);
+    }
+    @Test
+    public void testPlus() {
+        Rational plus = new Rational(1,2).plus(new Rational(1,3));
+        assertEquals(5, plus.getNumerator());
+        assertEquals(6, plus.getDenominator());
+    }
+    /*comment*/
+    @Test public void testMinus() {
+        Rational minus = new Rational(1,2).minus(new Rational(1,3));
+        assertEquals(1, minus.getNumerator());
+        assertEquals(6, minus.getDenominator());
     }
 }
