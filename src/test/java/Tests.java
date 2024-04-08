da2be3e9b4283bf0cf4dfef4199896b68fcd2b39
Alexander Demin <quilberion@gmail.com>
2024-04-08 15:21:49 +0300

    add create rational number test and mutiply test

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -8,4 +8,16 @@ public class Tests {
         assertEquals("Standard constructor returns wrong numerator", 0, standard.getNumerator());
         assertEquals("Standard constructor returns wrong denominator", 1, standard.getDenominator());
     }
+    @Test
+    public void testCreatRational() {
+        Rational s = new Rational(-1, 2);
+        assertEquals(-1, s.getNumerator());
+        assertEquals(2, s.getDenominator());
+    }
+    @Test
+    public void testMultiply() {
+        Rational s = new Rational(1,2).multiply(new Rational(1, 3));
+        assertEquals(1, s.getNumerator());
+        assertEquals(6, s.getDenominator());
+    }
 }
